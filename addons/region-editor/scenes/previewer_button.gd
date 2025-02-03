aaa7044f8d5546dadb9b6a86967a7daa6e2d5e18
Sabinayo <sabinayohiembo@gmail.com>
2025-02-03 23:29:47 +0000

    Update

--- addons/region-editor/scenes/previewer_button.gd
+++ addons/region-editor/scenes/previewer_button.gd
@@ -1,20 +1,23 @@
 @tool
 extends Button
 
+@export var drag_export_viewport_path: NodePath
 
 func _get_drag_data(at_position: Vector2) -> Variant:
-	var subviewport: SubViewport = $"../SubViewport"
-	#var preview: 
-	subviewport.size = $Preview.texture.get_size()
-	subviewport.add_child($Preview.duplicate())
-	await RenderingServer.frame_post_draw
-	
 	var image_path: String = "res://addons/region-editor/dragged_regions/%s.png" % get_parent().preview_name
-	var image: Image = subviewport.get_texture().get_image()
+	await RenderingServer.frame_post_draw
+	var image: Image = %DragPreview.get_texture().get_image()
 	image.save_png(image_path)
-	
+	await get_tree().process_frame
+	var filesystem: EditorFileSystem = EditorInterface.get_resource_filesystem()
+	filesystem.reimport_files([image_path])
+	await filesystem.resources_reimported
 	var drag_data: Dictionary = {
 		"type": "files",
 		"files": [image_path],
 	}
+	
+	#var drag_preview: TextureRect = preview.duplicate()
+	#drag_preview.size = Vector2(50, 50)
+	#set_drag_preview(drag_preview)
 	return drag_data
